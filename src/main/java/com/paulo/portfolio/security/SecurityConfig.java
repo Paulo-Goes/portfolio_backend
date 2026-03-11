eee8697df099bb9329af286234d3d68b0f7a8126
Paulo-Goes <paulosergiolobogoes@gmail.com>
2026-03-11 09:56:50 -0300

    Add filter rules

---
+++ src/main/java/com/paulo/portfolio/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.paulo.portfolio.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private JwtFilter jwtFilter;
+    
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf
+            .disable())
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(HttpMethod.GET, "/api/skills/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/projects/**").permitAll()
+
+                    .requestMatchers(HttpMethod.POST, "/api/contact").permitAll()
+
+                    .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+
+                    .anyRequest().authenticated()
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+    }
+}
